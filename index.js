8645af1ccd2406252560d764767da01aa9acf166
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2022-03-30 23:34:56 +0200

    arrow functions. edit task, filter list

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ let addTaskBtn = document.querySelector('.add-task-btn');
 let taskInput = document.querySelector('.task-input');
 let todoListRaw = document.querySelector('.todo-list-raw');
 let todoTask = document.querySelector('.todo-tasks');
-console.log("todoTask", todoTask)
-let todoList = []
+
+// FILTER
+let filterTaskBtn = document.querySelector('.filter-task-btn')
+let filterTaskInput = document.querySelector('.filter-task-input')
+
+filterTaskBtn.addEventListener('click',()=>{
+  let filterText = filterTaskInput.value
+  // let filteredArray = todoList.filter(function(todo){
+  //   console.log("Todo Item",todo)
+  // })
+
+  let filteredArray = todoList.filter(todo=>todo.text.toLowerCase().includes(filterText.toLowerCase()))   
+  console.log("Results", filteredArray)
+  todoList = filteredArray
+  updateList()
+})
+
+
+let todoList = [
+  {text:"Read a book", status:'todo'},
+  {text:"Buy groceries", status:'todo'},
+  {text:"Call the doctor", status:'todo'},
+  {text:"Learn JS", status:'todo'},
+  {text:"Update my CV", status:'todo'},
+]
 addTaskBtn.addEventListener('click',function(){
   console.log("Add task to list", taskInput.value)
   todoList.push({text:taskInput.value, status:'todo'})
@@ -11,17 +34,26 @@ addTaskBtn.addEventListener('click',function(){
   taskInput.value=''
 })
 
+
+
+let selectedTask
+
 function updateList(){
   todoTask.innerHTML = ''
   for(let i = 0; i < todoList.length; i++){
+    let todo = todoList[i]
     let li = document.createElement('li')
     let editBtn = document.createElement('button')
     let deleteBtn = document.createElement('button')
+    let completedBtn = document.createElement('button')
     let spanEl = document.createElement('span')
 
     editBtn.innerHTML = 'Edit'
     editBtn.addEventListener('click',function(){
-      console.log("Edit this item", todoList[i])
+      console.log("Edit this item", todo)
+      selectedTask = todo
+      taskEditInput.value = todo.text
+      // editTask(todoList[i])
     })
     deleteBtn.innerHTML = 'Delete'
     deleteBtn.addEventListener('click',function(){
@@ -29,9 +61,26 @@ function updateList(){
       todoList.splice(i,1)
       updateList()
     })
-    spanEl.innerHTML = todoList[i].text + ": " + todoList[i].status
+
+    completedBtn.innerHTML="⚡️"
+    completedBtn.addEventListener('click',function(){
+      console.log("Task completed",todoList[i])
+      todoList[i].status = "completed"
+      updateList()
+    })
+    completedBtn.classList.add('px-3','py-1','borderd-2','border-orange-500','rounded-md','bg-orange-100','hover:bg-orange-300')
+
+    spanEl.addEventListener('input',(evt)=>todoList[i].text = evt.target.innerHTML)
+    spanEl.addEventListener('blur',()=>{
+      console.log("You can update the list")
+      updateList()
+    })
+    spanEl.innerHTML = todoList[i].text 
+    spanEl.contentEditable = true
     spanEl.classList.add('flex-grow')
+    spanEl.classList.add(todoList[i].status=='completed' ? 'line-through': null)
     li.appendChild(spanEl)
+    li.appendChild(completedBtn)
     li.appendChild(editBtn)
     li.appendChild(deleteBtn)
     li.classList.add('flex','gap-3','w-full','h-12','bg-gray-200','border-b-2','border-gray-100','p-2','items-baseline')
@@ -43,6 +92,17 @@ function updateList(){
   //   todoTask.appendChild(li)
   // })
 }
+
+
+
+function editTask(task){
+  let newText = taskEditInput.value
+  selectedTask.text=newText
+  // task.text = newText
+  updateList()
+}
+
+updateList()
 
 // todoListRaw.innerHTML="HELLO"
 
