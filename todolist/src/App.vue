172102aef0ddf962092fbacf2222799ad9ffc01c
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2022-04-13 18:54:49 +0200

    Adds Axios. pokemon API

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import AppHeader from "./components/AppHeader.vue";
+import Pokedeck from "./components/Pokedeck.vue";
 let todoList = ref([
   {text:"Read a book", completed:false},
   {text:"Buy groceries", completed:false},
@@ -10,6 +11,7 @@ let todoList = ref([
 ])
 let newTask = ref('hello')
 const addTask=()=>{
+  console.log("Adding task...")
   todoList.value.push({text:newTask.value,completed:false})
   newTask.value=''
 }
@@ -25,18 +27,36 @@ const updateTask = (event,index)=>{
   console.log("Updating ",event.target.innerText,index)
   todoList.value[index].text=event.target.innerText
 }
+
+const update = (e)=>{
+  console.log("Updating",e)
+  newTask.value = e
+}
+
+let searchText = ref('')
+const filteredTodo = computed(()=>{
+  return todoList.value.filter(todo=>todo.text.toLowerCase().includes(searchText.value.toLowerCase()))
+})
 </script>
 
 <template>
 <app-header></app-header>
 <main class="container py-6">
-  <div>
-    <input v-model="newTask"  type="text" placeholder="Add todo" class="border-2 border-r-0 border-blue-300 rounded-lg rounded-r-none py-3 px-2 focus:outline-none">
-    <button class="border-2 border-l-0 border-blue-300 bg-blue-500 rounded-lg rounded-l-none py-3 px-4" @click="addTask">Add</button>
+  <!-- <div class="flex gap-12">
+    <my-input-text v-model="newTask" @input="update" @addtask="addTask"></my-input-text>
+    <div class="flex">
+      <input v-model="newTask"  type="text" placeholder="Add todo" class="border-2 border-r-0 border-blue-300 rounded-lg rounded-r-none py-3 px-2 focus:outline-none w-full">
+      <button class="border-2 border-l-0 border-blue-300 bg-blue-500 rounded-lg rounded-l-none py-3 px-4" @click="addTask">Add</button>
+    </div>
+    <div>
+      <input v-model="searchText"  type="text" placeholder="filter" class="border-2  border-blue-300 rounded-lg  py-3 px-2 focus:outline-none w-full">
+
+    </div>
   </div>
 
+
   <ul class="mt-12">
-    <li class="flex py-3 px-4 bg-gray-100 rounded-lg mb-2" v-for="(todo, t) in todoList" :key="t">
+    <li class="flex py-3 px-4 bg-gray-100 rounded-lg mb-2" v-for="(todo, t) in filteredTodo" :key="t">
       <span contenteditable="true" @input="updateTask($event,t)" class="flex-grow" :class="{'line-through':todo.completed}">{{todo.text}}</span>
       <div class="flex gap-4">
         <button @click="updateStatus(t)">⚡️</button>
@@ -44,7 +64,8 @@ const updateTask = (event,index)=>{
         <button @click="deleteTask(t)">🗑</button>
       </div>
     </li>
-  </ul>
+  </ul> -->
+  <pokedeck></pokedeck>
 </main>
 </template>
 
---
+++ todolist/src/components/MyInputText.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- HTML GOES HERE -->
+  <div class="flex">
+    <input v-model="newValue" type="text" placeholder="Add todo"
+      class="border-2 border-r-0 border-blue-300 rounded-lg rounded-r-none py-3 px-2 focus:outline-none w-full">
+    <button class="border-2 border-l-0 border-blue-300 bg-blue-500 rounded-lg rounded-l-none py-3 px-4"
+      @click="addTask">Add</button>
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+
+// JAVASCRIPT GOES HERE
+/**
+var props = {
+  modelValue
+}
+props.modelValue
+*/
+
+
+const props = defineProps({
+  modelValue:{
+    type:String,
+    default:'my input text model',
+  }
+})
+const emits = defineEmits(['input','addtask'])
+const newValue = computed({
+  get(){
+    return props.modelValue
+  },
+  set(v){
+    emits('input',v)
+  }
+})
+const addTask=()=>{
+  console.log("Emitting...")
+  emits('input',newValue)
+  emits('addtask')
+}
+</script>
+
+<style>
+/** CSS GOES HERE */
+</style>
